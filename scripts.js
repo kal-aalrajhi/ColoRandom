144d0ac70d8d3cd903b11b6cf0159afce7248e4d
Kal <kal.aalrajhi@gmail.com>
2022-02-22 18:02:59 -0700

    Add page load event listener to load random palette

--- scripts.js
+++ scripts.js
@@ -1,9 +1,18 @@
+// Global Variables
 var hexCharacters =[
   "1", "2", "3", "4", "5", "6", "7", "8", "9", "0",
   "A", "B", "C", "D", "E", "F"
 ]
 
-var currentPalette
+var currentPalette;
+
+// Event Listeners
+window.addEventListener('load', makeNewPalette);
+
+// Functions
+function getRandomIndex(array){
+  return Math.floor(Math.random() * array.length);
+}
 
 function makeNewHex() {
   var hexCode = "#";
@@ -13,17 +22,9 @@ function makeNewHex() {
    return new Color(hexCode);
 }
 
-
 function makeNewPalette(){
   currentPalette = new Palette();
   for(var i = 0; i < 5; i++){
     currentPalette[`color${i + 1}`] = makeNewHex();
   }
 }
-makeNewPalette()
-console.log(currentPalette)
-
-
-function getRandomIndex(array){
-  return Math.floor(Math.random() * array.length);
-};
